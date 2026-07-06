754540c8f103eb0ad0fd45e2195c00803a7a1073
zmd92918-oss <mengdan.zhao@hashicorp.com>
2026-07-06 17:41:13 +0900

    add vm

--- main.tf
+++ main.tf
@@ -37,3 +37,82 @@ resource "random_string" "suffix" {
   special = false
   upper   = false
 }
+
+# Public IP：EC2でいうElastic IP/パブリックIP
+resource "azurerm_public_ip" "demo" {
+  name                = "pip-handson-demo"
+  resource_group_name = azurerm_resource_group.demo.name
+  location            = azurerm_resource_group.demo.location
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+# Network Security Group：EC2でいうSecurity Group
+resource "azurerm_network_security_group" "demo" {
+  name                = "nsg-handson-demo"
+  resource_group_name = azurerm_resource_group.demo.name
+  location            = azurerm_resource_group.demo.location
+
+  security_rule {
+    name                       = "AllowSSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*" # デモ用。本番では自分のIPに絞る
+    destination_address_prefix = "*"
+  }
+}
+
+# NIC：EC2インスタンスに紐づくENIに相当
+resource "azurerm_network_interface" "demo" {
+  name                = "nic-handson-demo"
+  resource_group_name = azurerm_resource_group.demo.name
+  location            = azurerm_resource_group.demo.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.demo.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.demo.id
+  }
+}
+
+# NICとNSGの紐づけ（AWSだとSGをインスタンス作成時に直接指定できるが、Azureは別途アタッチが必要）
+resource "azurerm_network_interface_security_group_association" "demo" {
+  network_interface_id      = azurerm_network_interface.demo.id
+  network_security_group_id = azurerm_network_security_group.demo.id
+}
+
+# Linux VM本体：EC2インスタンスに相当
+resource "azurerm_linux_virtual_machine" "demo" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.demo.name
+  location            = azurerm_resource_group.demo.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+
+  network_interface_ids = [
+    azurerm_network_interface.demo.id,
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = var.ssh_public_key
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  # Ubuntu 22.04 LTS。AWSでいうAMIの指定に相当
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -35,3 +35,27 @@ variable "storage_account_prefix" {
   type        = string
   default     = "sthandsondemo"
 }
+
+variable "vm_name" {
+  description = "仮想マシン名（EC2相当）"
+  type        = string
+  default     = "vm-handson-demo"
+}
+
+variable "vm_size" {
+  description = "VMサイズ（EC2のinstance typeに相当）。B1sは最小・低コストクラス"
+  type        = string
+  default     = "Standard_B1s"
+}
+
+variable "admin_username" {
+  description = "VMの管理者ユーザー名"
+  type        = string
+  default     = "azureuser"
+}
+
+variable "ssh_public_key" {
+  description = "SSH公開鍵（~/.ssh/id_rsa.pub の中身。パスワード認証は使わずSSH鍵推奨）"
+  type        = string
+}
+
